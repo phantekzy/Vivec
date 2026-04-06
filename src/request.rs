fd2fd756dce8a3ff0af28187893bbcd7973e913c
phantekzy <mainilotfi@gmail.com>
2026-04-06 10:34:32 +0100

    added a public struct for the request

--- src/request.rs
+++ src/request.rs
@@ -1,7 +1,17 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 pub enum Method {
     GET,
     POST,
     PUT,
     DELETE,
+}
+
+#[derive(Debug)]
+pub struct Request {
+    pub method: Method,
+    pub url: String,
+    pub header: HashMap<String, String>,
+    pub body: Option<String>,
 }
