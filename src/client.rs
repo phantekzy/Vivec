ea434a08d88e0d8beba109da686997bcd74d2f73
phantekzy <mainilotfi@gmail.com>
2026-04-11 20:23:54 +0100

    i have to redo the client because i forgot what i was doing

--- src/client.rs
+++ src/client.rs
@@ -1,28 +1,9 @@
 use std::time::Duration;
 
-use crate::{
-    error::XsusError,
-    interceptor::Interceptors,
-    request::{Method, Request},
-    response::Response,
-};
+use crate::interceptor::Interceptors;
 
 pub struct Xsus {
     pub base_url: String,
     pub timeout: Duration,
     pub interceptors: Interceptors,
-}
-
-impl Xsus {
-    pub fn new(base_url: &str) -> Self {
-        Self {
-            base_url: base_url.to_string(),
-            timeout: Duration::from_secs(10),
-            interceptors: Interceptors::new(),
-        }
-    }
-    pub fn get(&self, path: &str) -> Result<Response, XsusError> {
-        let full_url = format!("{}{}", self.base_url, path);
-        let mut req = Request::new(Method::GET, &full_url);
-    }
 }
