c8c34672dc1ac0380b84cf6f888f8b74ad22248f
phantekzy <mainilotfi@gmail.com>
2026-04-11 20:26:22 +0100

    Setup  the constructor

--- src/client.rs
+++ src/client.rs
@@ -7,3 +7,13 @@ pub struct Xsus {
     pub timeout: Duration,
     pub interceptors: Interceptors,
 }
+
+impl Xsus {
+    pub fn new(base_url: &str) -> Self {
+        Self {
+            base_url: base_url.to_string(),
+            timeout: Duration::from_secs(10),
+            interceptors: Interceptors::new(),
+        }
+    }
+}
